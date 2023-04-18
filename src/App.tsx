3c48a2ec067a8467181680558e0488fdc1f285f2
Gabe Jensen <thegabejensen@gmail.com>
2023-04-18 07:31:15 -0700

    Tossed into a component

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,10 @@
 import { HopeProvider } from "@hope-ui/solid";
-import { Component, createResource, createSignal } from "solid-js";
-import Fetcher from "./Fetcher";
-import { Video } from "./ApiTypes";
-import Button from "./components/Button";
-import TextInput from "./components/TextInput";
-import Heading4 from "./components/Heading4";
+import { Component } from "solid-js";
 import { Tabs, TabList, Tab, TabPanel } from "@hope-ui/solid";
 import SearchTab from "./sections/SearchTab";
-import Form from "./components/Form";
-
-async function getVideos(): Promise<Array<Video>> {
-  const data = await Fetcher.get("/video/all");
-  const allUsers: Array<Video> = data;
-
-  return allUsers;
-}
+import AddVideoTab from "./sections/AddVideoTab";
 
 const App: Component = () => {
-  const [loading, setLoading] = createSignal(false);
-  const [allVideos, { refetch }] = createResource(getVideos);
-  const [url, setUrl] = createSignal("");
-
-  async function addVideo() {
-    setLoading(true);
-
-    await Fetcher.post(
-      "/video",
-      {},
-      {
-        url: url(),
-      }
-    );
-
-    setLoading(false);
-
-    setUrl("");
-    refetch();
-  }
-
   return (
     <HopeProvider>
       <div class="p-6 bg-gray-50 min-h-screen">
@@ -54,24 +21,7 @@ const App: Component = () => {
               <Tab>Search Videos</Tab>
             </TabList>
             <TabPanel>
-              <span>Paste the URL:</span>
-              <Form onSubmit={addVideo}>
-                <div class="flex flex-col md:flex-row">
-                  <TextInput
-                    className="w-full md:w-[400px]"
-                    placeholder="https://www.youtube.com/watch?v=2C_F92QmT88"
-                    onInput={(e) => setUrl(e.target.value)}
-                    value={url()}
-                    disabled={loading() || allVideos.loading}
-                  />
-                  <Button
-                    className="ml-0 md:ml-3 mt-2 md:mt-0 w-20"
-                    loading={loading() || allVideos.loading}
-                  >
-                    Add
-                  </Button>
-                </div>
-              </Form>
+              <AddVideoTab />
             </TabPanel>
             <TabPanel>
               <SearchTab />
---
+++ src/sections/AddVideoTab.tsx
@@ -0,0 +1,51 @@
+import Button from "../components/Button";
+import { Component, createResource, createSignal } from "solid-js";
+import TextInput from "../components/TextInput";
+import Form from "../components/Form";
+import Fetcher from "../Fetcher";
+
+type AddVideoTabProps = {};
+
+export default function AddVideoTab(props: AddVideoTabProps) {
+  const [loading, setLoading] = createSignal(false);
+  const [url, setUrl] = createSignal("");
+
+  async function addVideo() {
+    setLoading(true);
+
+    await Fetcher.post(
+      "/video",
+      {},
+      {
+        url: url(),
+      }
+    );
+
+    setLoading(false);
+
+    setUrl("");
+  }
+
+  return (
+    <div class="AddVideoTab">
+      <span>Paste the URL:</span>
+      <Form onSubmit={addVideo}>
+        <div class="flex flex-col md:flex-row">
+          <TextInput
+            className="w-full md:w-[400px]"
+            placeholder="https://www.youtube.com/watch?v=2C_F92QmT88"
+            onInput={(e) => setUrl(e.target.value)}
+            value={url()}
+            disabled={loading()}
+          />
+          <Button
+            className="ml-0 md:ml-3 mt-2 md:mt-0 w-20"
+            loading={loading()}
+          >
+            Add
+          </Button>
+        </div>
+      </Form>
+    </div>
+  );
+}
